41172d22deb77e49cb677b505193ac2406fbb9a9
Focal Fossa <69269521+FocalFossa0997@users.noreply.github.com>
2021-04-06 21:56:51 -0700

    Update palmtree-test.py

--- apps/palmtree-test/palmtree-test.py
+++ apps/palmtree-test/palmtree-test.py
@@ -1,2 +1,12 @@
 import os
-# this isn't complete...
+import time
+
+print("Welcome to the palmtree-test setup application!")
+print("This should setup palmtree-test and run it automatically!")
+print("Now installing needed dependencies for palmtree-test")
+time.sleep(1)
+print("Now installing zenity")
+os.system("sudo apt-get install zenity")
+os.system("wget https://raw.githubusercontent.com/FocalFossa0997/palmtree-apps/main/apps/palmtree-test/test.txt")
+os.system("zenity --title='palmtree-test (test.txt)' --text-info --filename='test.txt'")
+os.system("rm test.txt")
